1a8bd44629516959b07311b58861b05cdc0ab7b9
Srinivasan Thoyyeti <srinivasan.thoyyeti@happiestminds.com>
2021-01-20 10:07:40 +0530

    init version

---
+++ src/test/java/com/hm/internal/ms/demo/zuulGateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.internal.ms.demo.zuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
